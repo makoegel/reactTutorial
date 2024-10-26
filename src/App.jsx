99ebfceb6ff7311a28c7c027d9ac9b2f42cfac1c
makoegel <marion-koegel@gmx.de>
2024-10-26 23:08:22 +0200

    derive and output data based on state with initial state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {CORE_CONCEPTS} from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import {EXAMPLES} from "./data-with-examples.js";
 
 function App() {
     /*call Hook-Functions always on the top level of the function
@@ -14,7 +15,7 @@ function App() {
     * const [counter, setCounter] = useState(0);
     * const is recreated every time it is used
     * */
-    const [selectedTopic, setSelectedTopic]= useState('Please click a button');
+    const [selectedTopic, setSelectedTopic] = useState('components');
 
     function handleSelect(selectedButton) {
         /*selectedButton => 'components', 'jsx', 'props', 'state*/
@@ -47,7 +48,15 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-                    {selectedTopic}
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>
+                                {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
