cc68368b541f56d6c7b2de1f156ffe00982eed5a
makoegel <marion-koegel@gmx.de>
2024-10-26 21:29:20 +0200

    read props from array CORE_CONCEPTS in data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+//named Import with curly braces
+import {CORE_CONCEPTS} from "./data.js";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,10 +23,10 @@ function Header() {
     );
 }
 
-function CoreConcept(props){
+function CoreConcept(props) {
     return (
         <li>
-            <img src={props.image} alt={props.title} />
+            <img src={props.image} alt={props.title}/>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
         </li>
@@ -40,13 +41,26 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept title="Components"
-                                     description="The core UI building block."
-                                     image={componentsImg}
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
                         />
-                        <CoreConcept />
-                        <CoreConcept/>
-                        <CoreConcept/>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            image={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            image={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}
+                        />
                     </ul>
                 </section>
             </main>
