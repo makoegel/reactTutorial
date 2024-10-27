b07cb372551178abb3cb6c3a84482d4b0c1bc64e
makoegel <marion-koegel@gmx.de>
2024-10-27 19:45:48 +0100

    css styling & dynamic styling for activ tabbutton

--- src/App.jsx
+++ src/App.jsx
@@ -58,10 +58,18 @@ function App() {
                     <menu>
                         {/*Component-Composition*/}
                         {/*anonymous function is executed when button is clicked*/}
-                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'components'}
+                            onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'jsx'}
+                            onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'props'}
+                            onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'state'}
+                            onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
                     {tabContent}
                 </section>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,14 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
 
     return (
         <li>
             {/*handlclick without parantheses: React executes first, when button is clicked
              = passing as value
             */}
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            {/*<button className={isSelected ? 'active' : ' '} onClick={onSelect}>{children}</button>*/}
+
+
         </li>
     );
 }
